c9a46ebd61f44a6f86a7e91f44be2a844fdea02d
Udayk16 <udaykumargovindu16@gmail.com>
2026-02-10 21:30:26 +0530

    new main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import AppRoutes from "./routes/AppRoutes";
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Trainers from "./pages/Trainers/Trainers";
+import Gallery from "./pages/Gallery/Gallery";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
   return (
     <>
       <Header />
-      <AppRoutes />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/trainers" element={<Trainers />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
       <Footer />
     </>
   );
