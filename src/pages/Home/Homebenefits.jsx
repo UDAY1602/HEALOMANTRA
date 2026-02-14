3b6ee0f00951d0aacb85811cb57acf5aab793487
UdayGoshike <udaygoshike2@gmail.com>
2026-02-14 18:58:36 +0530

    sec2 done

--- src/pages/Home/Homebenefits.jsx
+++ src/pages/Home/Homebenefits.jsx
@@ -1,11 +1,47 @@
 import React from "react";
+import "./benefits.css";
+
+import flexibility from "../../assets/home_assets/Flexibility.png";
+import balance from "../../assets/home_assets/Balance.png";
+import calm from "../../assets/home_assets/Calm.png";
+
+
 
 const Homebenefits = () => {
-  return (
-    <section className="home-benefits">
-      <h2>Benefits Section</h2>
+    return (
+    <section className="benefits-section">
+      
+      <h2 className="benefits-title">
+        Your <span>Yoga Journey</span> Benefits
+      </h2>
+
+      <p className="benefits-subtitle">
+        A gentle path toward inner peace, physical vitality, and mindful
+        living guiding you to reconnect with yourself.
+      </p>
+
+      <div className="benefits-container">
+
+        <div className="benefit-card">
+          <img src={flexibility} alt="Flexibility" />
+          
+        </div>
+
+        <div className="benefit-card">
+          <img src={balance} alt="Balance" />
+          
+        </div>
+
+        <div className="benefit-card">
+          <img src={calm} alt="Calm" />
+          
+        </div>
+
+      </div>
+
     </section>
   );
+
 };
 
 export default Homebenefits;
