6b9629aebe14d9a9760b6dc5313c235e0d6dde3c
Pranav-Sagar <dyapapranav@gmail.com>
2026-02-14 13:27:02 +0530

    Made Navbar sticky and Menu for mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,46 +17,48 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="nav-wrapper">
-      <div className="nav-content">
+    <>
+      <div className="nav-wrapper">
+        <div className="nav-content">
 
-        <div className="brand">
-          <img src={logo} alt="logo" className="nav-logo" />
+          <div className="brand">
+            <img src={logo} alt="logo" className="nav-logo" />
+            {isMobile && (
+              <span className="brand-name">Heal O Mantra</span>
+            )}
+          </div>
 
-         
-          {isMobile && (
-            <span className="brand-name">Heal O Mantra</span>
-          )}
+          <div className="nav-container">
+            <NavLink to="/" className="nav-link">Home</NavLink>
+            <NavLink to="/about" className="nav-link">About</NavLink>
+            <NavLink to="/trainers" className="nav-link">Trainers</NavLink>
+            <NavLink to="/contact" className="nav-link">Contact</NavLink>
+            <NavLink to="/gallery" className="nav-link">Gallery</NavLink>
+          </div>
+
+          <div
+            className={`hamburger ${menuOpen ? "active" : ""}`}
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+
         </div>
 
-        <div className="nav-container">
-          <NavLink to="/" className="nav-link">Home</NavLink>
-          <NavLink to="/about" className="nav-link">About</NavLink>
-          <NavLink to="/trainers" className="nav-link">Trainers</NavLink>
-          <NavLink to="/contact" className="nav-link">Contact</NavLink>
-          <NavLink to="/gallery" className="nav-link">Gallery</NavLink>
+        <div className={`mobile-menu ${menuOpen ? "open" : ""}`}>
+          <NavLink to="/" onClick={() => setMenuOpen(false)}>Home</NavLink>
+          <NavLink to="/about" onClick={() => setMenuOpen(false)}>About</NavLink>
+          <NavLink to="/trainers" onClick={() => setMenuOpen(false)}>Trainers</NavLink>
+          <NavLink to="/contact" onClick={() => setMenuOpen(false)}>Contact</NavLink>
+          <NavLink to="/gallery" onClick={() => setMenuOpen(false)}>Gallery</NavLink>
         </div>
-
-        <div 
-          className="hamburger"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-
       </div>
 
-      <div className={`mobile-menu ${menuOpen ? "open" : ""}`}>
-        <NavLink to="/" onClick={() => setMenuOpen(false)}>Home</NavLink>
-        <NavLink to="/about" onClick={() => setMenuOpen(false)}>About</NavLink>
-        <NavLink to="/trainers" onClick={() => setMenuOpen(false)}>Trainers</NavLink>
-        <NavLink to="/contact" onClick={() => setMenuOpen(false)}>Contact</NavLink>
-        <NavLink to="/gallery" onClick={() => setMenuOpen(false)}>Gallery</NavLink>
-      </div>
-
-    </div>
+      {/* Spacer to prevent content hiding under fixed navbar */}
+      <div className="navbar-spacer"></div>
+    </>
   );
 };
 
