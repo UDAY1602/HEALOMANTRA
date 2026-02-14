49dd95f9eb9908ada5236dadf1166eed37a85ad7
Teja <tejaswavarma@gmail.com>
2026-02-14 16:47:44 +0530

    added welcome to healomantra and why healomantra

--- src/pages/Home/Homewhy.jsx
+++ src/pages/Home/Homewhy.jsx
@@ -1,23 +1,25 @@
 import React from "react";
+import "./Homewhy.css";
+
+import ideaIcon from "../../assets/home_assets/ideahandicon.png";
+import flowerIcon from "../../assets/home_assets/handflowericon.png";
+import meditateIcon from "../../assets/home_assets/meditatingicon.png";
 
 const Homewhy = () => {
   return (
     <section className="why-wrapper">
       <div className="why-section">
-        <h2 className="why-title">
+        <h2 className="why-title poppins-semibold">
           Why <span>Heal O Mantra</span>
         </h2>
 
-        <div className="why-container">
+        <div className="why-container poppins-regular">
 
-          <div className="why-card">
+          <div className="why-card ">
             <div className="why-icon">
-              {/* Add your PNG here later */}
-              <div className="icon-placeholder"></div>
+              <img src={ideaIcon} alt="Holistic Approach" />
             </div>
-
             <h3>Holistic Approach</h3>
-
             <p>
               At Heal O Mantra, we go beyond physical postures by integrating
               movement, breath, and mindfulness to support overall well-being
@@ -27,11 +29,9 @@ const Homewhy = () => {
 
           <div className="why-card">
             <div className="why-icon">
-              <div className="icon-placeholder"></div>
+              <img src={flowerIcon} alt="Expert Guidance" />
             </div>
-
             <h3>Expert Guidance</h3>
-
             <p>
               Our experienced instructors provide thoughtful, personalized
               guidance, ensuring each practice is safe, purposeful, and aligned
@@ -41,11 +41,9 @@ const Homewhy = () => {
 
           <div className="why-card">
             <div className="why-icon">
-              <div className="icon-placeholder"></div>
+              <img src={meditateIcon} alt="Peaceful Environment" />
             </div>
-
             <h3>Peaceful Environment</h3>
-
             <p>
               We offer a serene and welcoming space that encourages relaxation,
               self-discovery, and a deeper connection with yourself.
