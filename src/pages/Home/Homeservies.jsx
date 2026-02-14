4747f13ee5913614fee20166bf4d05cf2fa7d861
Teja <tejaswavarma@gmail.com>
2026-02-14 12:51:32 +0530

    structured home page files

---
+++ src/pages/Home/Homeservies.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homeservies = () => {
+  return (
+    <section className="home-services">
+      <h2>Services Section</h2>
+    </section>
+  );
+};
+
+export default Homeservies;
---
+++ src/pages/Home/Homewelcome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homewelcome = () => {
+  return (
+    <section className="home-welcome">
+      <h2>Welcome Section</h2>
+    </section>
+  );
+};
+
+export default Homewelcome;
